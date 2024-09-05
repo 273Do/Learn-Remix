da0efc3b88e330359f2037aff9c5ff3e9f3ade39
273* <keita.y310@icloud.com>
2024-09-05 12:37:48 +0900

    ルーティング設定

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -66,6 +66,10 @@ export default function Index() {
           </div>
         </div>
 
+        <div className="mt-16 text-center text-blue-400">
+          <Link to="/posts">Blog Posts</Link>
+        </div>
+
         <div className="mx-auto max-w-7xl px-4 py-2 sm:px-6 lg:px-8">
           <div className="mt-6 flex flex-wrap justify-center gap-8">
             {[
---
+++ app/routes/posts._index.tsx
@@ -0,0 +1,10 @@
+// ._indexを入れると，ページの共通レイアウトではない部分を実装するときに使う
+// headerやfooterの時は何もつけない
+// つけなくても動くがつけた方がいい
+export default function Posts() {
+  return (
+    <main>
+      <h1>Posts</h1>
+    </main>
+  );
+}
